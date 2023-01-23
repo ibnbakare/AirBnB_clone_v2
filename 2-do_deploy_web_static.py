14423b419e03cee18610a59cc2e043c3fe63969a
ridhwan <ridmartrite@gmail.com>
2023-01-23 10:03:47 -0500

    adding task 3 again

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['100.24.74.197', '52.91.117.197']
+env.hosts = ['100.25.144.66', '54.157.191.121']
 
 
 def do_deploy(archive_path):
